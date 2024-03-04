b6b4aaeaaa816ae7ca78ae6d5406be1769e5cbd3
fsorodrigues <fsorodrigues@gmail.com>
2024-03-04 14:40:24 +1300

    Switch to harpoon2

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -1,24 +1,81 @@
 return {
   "ThePrimeagen/harpoon",
+  branch = "harpoon2",
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
   config = function()
-    -- set keymaps
+    local harpoon = require("harpoon")
     local keymap = vim.keymap -- for conciseness
+    harpoon:setup()
 
-    keymap.set(
-      "n",
-      "<leader>hm",
-      "<cmd>lua require('harpoon.mark').add_file()<cr>",
-      { desc = "Mark file with harpoon" }
-    )
-    keymap.set("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", { desc = "Go to next harpoon mark" })
-    keymap.set(
-      "n",
-      "<leader>hp",
-      "<cmd>lua require('harpoon.ui').nav_prev()<cr>",
-      { desc = "Go to previous harpoon mark" }
-    )
+    -- basic telescope configuration
+    local conf = require("telescope.config").values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      local make_finder = function()
+        local paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(paths, item.value)
+        end
+
+        return require("telescope.finders").new_table({
+          results = paths,
+        })
+      end
+
+      require("telescope.pickers")
+        .new({}, {
+          prompt_title = "Harpoon",
+          finder = require("telescope.finders").new_table({
+            results = file_paths,
+          }),
+          previewer = conf.file_previewer({}),
+          sorter = conf.generic_sorter({}),
+          attach_mappings = function(prompt_buffer_number, map)
+            map(
+              "i",
+              "dd", -- your mapping here
+              function()
+                local state = require("telescope.actions.state")
+                local selected_entry = state.get_selected_entry()
+                local current_picker = state.get_current_picker(prompt_buffer_number)
+
+                harpoon:list():removeAt(selected_entry.index)
+                current_picker:refresh(make_finder())
+              end
+            )
+
+            return true
+          end,
+        })
+        :find()
+    end
+
+    -- set telescope toggle keymap
+    keymap.set("n", "<leader>hl", function()
+      toggle_telescope(harpoon:list())
+    end, { desc = "Open harpoon window" })
+
+    -- set other keymaps
+    keymap.set("n", "<leader>hm", function()
+      harpoon:list():append()
+    end, { desc = "Mark file with harpoon" })
+
+    keymap.set("n", "<leader>hum", function()
+      harpoon:list():remove()
+    end, { desc = "Unmark file with harpoon" })
+
+    keymap.set("n", "<leader>hn", function()
+      harpoon:list():next()
+    end, { desc = "Go to next harpoon mark" })
+
+    keymap.set("n", "<leader>hp", function()
+      harpoon:list():prev()
+    end, { desc = "Go to previous harpoon mark" })
   end,
 }
