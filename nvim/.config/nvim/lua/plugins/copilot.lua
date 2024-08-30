5719d884596d83d5e29be92a42136ea24ff82d82
fsorodrigues <fsorodrigues@gmail.com>
2024-08-30 14:55:02 +1200

    Change Copilot defaults
    Disable GitHub Copilot by default and add a keymap to allow for a explicit suggestion request.

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,7 @@
 return {
   "github/copilot.vim",
+  config = function()
+    vim.g.copilot_enabled = false
+    vim.keymap.set("i", "<C-c>", "<Plug>(copilot-suggest)", { noremap = false })
+  end,
 }
