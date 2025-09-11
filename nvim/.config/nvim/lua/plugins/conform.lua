0fcacdda5bf4c3e2b74e8c428e0a556510380a9b
felippe.rodrigues <felippe.rodrigues@ptml.com>
2025-09-12 08:50:18 +1200

    feat: making sqlfluff work with nvim-lint & conform
    nvim-lint: it needed to honor the .sqlfluff config file in the cwd, but
    for some reason it wasn't. adding some custom args solved the problem.
    
    conform: adding the formatter for sql filetype

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -7,6 +7,6 @@ return {
     format_on_save = {
       timeout_ms = 5000,
     },
-    default_format_opts = { async = true }
+    default_format_opts = { async = true },
   },
 }
--- nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -6,6 +6,12 @@ return {
   },
   config = function()
     local lint = require("lint")
+    lint.linters.sqlfluff.args = {
+      "lint",
+      "--format=json",
+      "--config=.sqlfluff",
+    }
+
     lint.linters_by_ft = {
       sql = { "sqlfluff" },
     }
@@ -17,5 +23,5 @@ return {
           lint.try_lint()
         end,
       })
-  end
+  end,
 }
