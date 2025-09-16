d012437985a356c60c3845ace295c452d0832d15
fsorodrigues <fsorodrigues@gmail.com>
2025-09-16 18:27:41 +1200

    feat: add go lsp config

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -1,3 +1,4 @@
+-- LUA
 vim.lsp.config("lua_ls", {
 	settings = {
 		Lua = {
@@ -11,10 +12,39 @@ vim.lsp.config("lua_ls", {
 	},
 })
 
+-- GO
+vim.lsp.config("gopls", {
+	settings = {
+		gopls = {
+			completeUnimported = true,
+			usePlaceholders = true,
+			analyses = {
+				unusedparams = true,
+			},
+		},
+	},
+	on_attach = function(_, bufnr)
+		-- Enable organizing imports on save
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = vim.api.nvim_create_augroup("GoFormat", { clear = true }),
+			buffer = bufnr,
+			callback = function()
+				vim.lsp.buf.code_action({
+					apply = true,
+					context = {
+						only = { "source.organizeImports" },
+						diagnostics = {},
+					},
+					bufnr = bufnr,
+				})
+			end,
+		})
+	end,
+})
+
 vim.lsp.enable({
 	"lua_ls",
 	"gopls",
-  "svelte",
-  "pyright",
+	"svelte",
+	"pyright",
 })
-
