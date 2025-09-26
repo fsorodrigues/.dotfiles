2c90294378ffdaeaf5ade91843b2dc1885851281
felippe.rodrigues <felippe.rodrigues@ptml.com>
2025-09-26 17:28:24 +1200

    refactor: some telescope quality of life settings

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -21,8 +21,20 @@ return {
           i = {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+            ["<C-v>"] = false,
+            ["<C-s>"] = actions.file_vsplit,
+            ["<C-h>"] = actions.file_split,
           },
+        },
+        layout_config = {
+          width = 0.95,
+          height = 0.95,
+          prompt_position = "top",
+          preview_width = 0.4,
+        },
+        preview = {
+          filesize_limit = 5,
         },
       },
     })
@@ -33,16 +45,32 @@ return {
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set(
+      "n",
+      "<leader>ff",
+      "<cmd>Telescope find_files<cr>",
+      { desc = "Fuzzy find files in cwd" }
+    )
     keymap.set(
       "n",
       "<leader>fa",
       "<cmd>Telescope find_files hidden=true<cr>",
       { desc = "Fuzzy find files in cwd, including hidden files" }
     )
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+    keymap.set(
+      "n",
+      "<leader>fr",
+      "<cmd>Telescope oldfiles<cr>",
+      { desc = "Fuzzy find recent files" }
+    )
     keymap.set("n", "<leader>fs", function()
       telescope.extensions.live_grep_args.live_grep_args()
     end, { desc = "Find string in cwd with ripgrep args" })
+    keymap.set(
+      "n",
+      "<leader>fc",
+      "<cmd>Telescope grep_string<cr>",
+      { desc = "Find string under cursor in cwd" }
+    )
   end,
 }
