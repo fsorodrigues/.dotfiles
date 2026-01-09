a17d2b49e0ad275ee70a37bf66c985f1deeaa84b
felippe.rodrigues <felippe.rodrigues@ptml.com>
2026-01-09 15:14:11 +1300

    feat: lualine changes to look more like tokyonight

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -4,21 +4,20 @@ return {
   config = function()
     local lualine = require("lualine")
     local lazy_status = require("lazy.status") -- to configure lazy pending updates count
-    local tokyonight = require("tokyonight.colors").setup({style = 'storm'})
+    local tokyonight = require("tokyonight.colors").setup({ style = "storm" })
 
     local colors = {
-      blue = "#65D1FF",
-      green = "#3EFFDC",
-      violet = "#FF61EF",
-      yellow = "#FFDA7B",
-      red = "#FF4A4A",
+      blue = tokyonight.blue, -- #7aa2f7
+      green = tokyonight.green1, -- #73daca
+      purple = tokyonight.purple, -- #9d7cd8
+      yellow = tokyonight.yellow, -- "#e0af68"
+      red = tokyonight.red1, -- "#db4b4b"
       fg = "#c3ccdc",
-      -- bg = "#112638",
       bg = tokyonight.bg_dark,
       inactive_bg = "#2c3043",
     }
 
-    local my_lualine_theme = {
+    local custom_theme = {
       normal = {
         a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
         b = { bg = colors.bg, fg = colors.fg },
@@ -30,7 +29,7 @@ return {
         c = { bg = colors.bg, fg = colors.fg },
       },
       visual = {
-        a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
+        a = { bg = colors.purple, fg = colors.bg, gui = "bold" },
         b = { bg = colors.bg, fg = colors.fg },
         c = { bg = colors.bg, fg = colors.fg },
       },
@@ -54,14 +53,15 @@ return {
     -- configure lualine with modified theme
     lualine.setup({
       options = {
-        theme = my_lualine_theme,
+        theme = custom_theme,
       },
+      extensions = { "quickfix" },
       sections = {
         lualine_x = {
           {
             lazy_status.updates,
             cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" },
+            color = { fg = colors.yellow },
           },
           { "encoding" },
           { "fileformat" },
