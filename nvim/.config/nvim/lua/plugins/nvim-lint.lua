a892c1ae0a63a6722b0a76cceb0c28e557ac7092
fsorodrigues <fsorodrigues@gmail.com>
2026-01-22 19:06:00 +1300

    feat: commenting it out for now

--- nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -6,21 +6,21 @@ return {
 	},
 	config = function()
 		local lint = require("lint")
-		lint.linters.sqlfluff.args = {
-			"lint",
-			"--format=json",
-			"--config=.sqlfluff",
-		}
-
-		lint.linters_by_ft = {
-			sql = { "sqlfluff" },
-		}
-
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
-			group = vim.api.nvim_create_augroup("nvim-lint-autocmds", { clear = true }),
-			callback = function()
-				lint.try_lint()
-			end,
-		})
+		-- lint.linters.sqlfluff.args = {
+		-- 	"lint",
+		-- 	"--format=json",
+		-- 	"--config=.sqlfluff",
+		-- }
+		--
+		-- lint.linters_by_ft = {
+		-- 	sql = { "sqlfluff" },
+		-- }
+		--
+		-- vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
+		-- 	group = vim.api.nvim_create_augroup("nvim-lint-autocmds", { clear = true }),
+		-- 	callback = function()
+		-- 		lint.try_lint()
+		-- 	end,
+		-- })
 	end,
 }
