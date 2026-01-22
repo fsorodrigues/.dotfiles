de9218a03ced0e88a7b094adcde0c3b41e22ac24
fsorodrigues <fsorodrigues@gmail.com>
2026-01-22 19:06:18 +1300

    chore: just formatting

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,25 +1,27 @@
 return {
-  "stevearc/oil.nvim",
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  lazy = false,
-  opts = {
-    view_options = {
-      show_hidden = true,
-    },
-    float = {
-      padding = 2,
-      max_width = 0.8,
-      max_height = 0.8,
-      border = "rounded",
-    },
-    default_file_explorer = true,
-    keymaps = {
-      ["<C-l>"] = false,
-      ["<C-r>"] = "actions.refresh",
-    }
-  },
-  config = function(_, opts)
-    vim.keymap.set("n", "<leader>ee", "<CMD>Oil --float<CR>", { desc = "Toggle Oil file navigator" })
-    require("oil").setup(opts)
-  end
+	"stevearc/oil.nvim",
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	lazy = false,
+	opts = {
+		view_options = {
+			show_hidden = true,
+		},
+		float = {
+			padding = 2,
+			max_width = 0.8,
+			max_height = 0.8,
+			border = "rounded",
+		},
+		default_file_explorer = true,
+		keymaps = {
+			["<C-l>"] = false,
+			["<C-r>"] = "actions.refresh",
+			["<C-c>"] = false,
+			["<ESC>"] = { "actions.close", mode = "n" },
+		},
+	},
+	config = function(_, opts)
+		vim.keymap.set("n", "<leader>ee", "<CMD>Oil --float<CR>", { desc = "Toggle Oil file navigator" })
+		require("oil").setup(opts)
+	end,
 }
