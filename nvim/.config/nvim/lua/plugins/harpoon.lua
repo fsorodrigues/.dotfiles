cd47581a76313cbce86daeb085900a320aa74c9d
fsorodrigues <felippe.rodrigues@ptml.com>
2026-03-24 16:47:21 +1300

    feat(nvim): add harpoonex plugin

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -3,79 +3,64 @@ return {
   branch = "harpoon2",
   dependencies = {
     "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "mike-jl/harpoonEx",
   },
   config = function()
     local harpoon = require("harpoon")
     local keymap = vim.keymap -- for conciseness
     harpoon:setup()
 
-    -- basic telescope configuration
-    local conf = require("telescope.config").values
-    local function toggle_telescope(harpoon_files)
-      local file_paths = {}
+    local harpoonEx = require("harpoonEx")
+    -- load extension
+    harpoon:extend(harpoonEx.extend())
 
-      for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-      end
-
-      local make_finder = function()
-        local paths = {}
-        for _, item in ipairs(harpoon_files.items) do
-          table.insert(paths, item.value)
-        end
-
-        return require("telescope.finders").new_table({
-          results = paths,
-        })
-      end
-
-      require("telescope.pickers")
-        .new({}, {
-          prompt_title = "Harpoon",
-          finder = require("telescope.finders").new_table({
-            results = file_paths,
-          }),
-          previewer = conf.file_previewer({}),
-          sorter = conf.generic_sorter({}),
-          attach_mappings = function(prompt_buffer_number, map)
-            map(
-              "i",
-              "dd", -- your mapping here
-              function()
-                local state = require("telescope.actions.state")
-                local selected_entry = state.get_selected_entry()
-                local current_picker =
-                  state.get_current_picker(prompt_buffer_number)
-
-                harpoon:list():remove_at(selected_entry.index)
-                current_picker:refresh(make_finder())
-              end
-            )
-
-            return true
-          end,
-        })
-        :find()
-    end
-    -- set telescope toggle keymap
-    keymap.set("n", "<leader>hl", function()
-      toggle_telescope(harpoon:list())
+    -- set Telescope integration
+    vim.keymap.set("n", "<leader>hl", function()
+      require("telescope").extensions.harpoonEx.harpoonEx({
+        -- Optional: modify mappings, default mappings:
+        attach_mappings = function(_, map)
+          local actions = require("telescope").extensions.harpoonEx.actions
+          map({ "i", "n" }, "<C-d>", actions.delete_mark)
+          map({ "i", "n" }, "<M-k>", actions.move_mark_up)
+          map({ "i", "n" }, "<M-j>", actions.move_mark_down)
+          return true
+        end,
+      })
+      return true
     end, { desc = "Open harpoon window" })
 
     -- set other keymaps
-    keymap.set("n", "<leader>hm", function()
+    keymap.set("n", "<leader>hh", function()
       harpoon:list():add()
     end, { desc = "Mark file with harpoon" })
 
-    keymap.set("n", "<leader>hum", function()
+    keymap.set("n", "<leader>hg", function()
       harpoon:list():remove()
     end, { desc = "Unmark file with harpoon" })
 
+    -- go to file by index
+    keymap.set("n", "<C-1>", function()
+      harpoon:list():select(1)
+    end, { desc = "Go to file 1 in harpoon list" })
+    keymap.set("n", "<C-2>", function()
+      harpoon:list():select(2)
+    end, { desc = "Go to file 2 in harpoon list" })
+    keymap.set("n", "<C-3>", function()
+      harpoon:list():select(3)
+    end, { desc = "Go to file 3 in harpoon list" })
+    keymap.set("n", "<C-4>", function()
+      harpoon:list():select(4)
+    end, { desc = "Go to file 4 in harpoon list" })
+    keymap.set("n", "<C-5>", function()
+      harpoon:list():select(5)
+    end, { desc = "Go to file 5 in harpoon list" })
+
+    -- cycle through files in order
     local opts = { ui_nav_wrap = true }
     keymap.set("n", "<leader>hn", function()
       harpoon:list():next(opts)
     end, { desc = "Go to next harpoon mark" })
-
     keymap.set("n", "<leader>hy", function()
       harpoon:list():prev(opts)
     end, { desc = "Go to previous harpoon mark" })
