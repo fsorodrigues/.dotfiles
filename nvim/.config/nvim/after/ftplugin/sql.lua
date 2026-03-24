1fa2ae0e100f552ed8c00d4cf880ec612c3022ee
fsorodrigues <felippe.rodrigues@ptml.com>
2026-03-24 16:42:42 +1300

    feat(nvim): useful macros for sql
    these macros are a superpower. conver

--- nvim/.config/nvim/after/ftplugin/sql.lua
+++ nvim/.config/nvim/after/ftplugin/sql.lua
@@ -7,7 +7,8 @@ opt.tabstop = 4 -- 4 spaces for tabs (sqlfluff rule)
 opt.shiftwidth = 4 -- 4 spaces for indent width
 
 -- registering macros that are useful
-vim.fn.setreg(
-  "a",
-  "yiwopV:s/\\([a-z]\\)\\([A-Z]\\)/\\1_\\l\\2/g\VgukA AS jkJ nh"
-)
+-- transform from ColumnName to column_name, make if an alias
+vim.fn.setreg("a", [[yiwopcr_ysiwkJi ASj]])
+
+-- transform from column_name to ColumnName, make if an alias
+vim.fn.setreg("s", [[yiwopcrpysiw"kJi ASj]])
