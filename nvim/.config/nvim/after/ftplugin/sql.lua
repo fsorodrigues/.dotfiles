59270eec27397fc29000a2109c557645d2d5794c
fsorodrigues <felippe.rodrigues@ptml.com>
2026-05-20 09:19:36 +1200

    chore: correct typo

--- nvim/.config/nvim/after/ftplugin/sql.lua
+++ nvim/.config/nvim/after/ftplugin/sql.lua
@@ -7,10 +7,10 @@ opt.tabstop = 4 -- 4 spaces for tabs (sqlfluff rule)
 opt.shiftwidth = 4 -- 4 spaces for indent width
 
 -- registering macros that are useful
--- transform from ColumnName to column_name, make if an alias
+-- transform from ColumnName to column_name, make alias
 vim.fn.setreg("a", [[yiwopcr_ysiwkJi ASj]])
 
--- transform from column_name to ColumnName, make if an alias
+-- transform from column_name to ColumnName, make alias
 vim.fn.setreg("s", [[yiwopcrpysiw"kJi ASj]])
 
 -- transform from to "ColumnName" to "Column Name"
