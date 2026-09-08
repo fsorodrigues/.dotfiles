0fbc8306b5c3092e367d718afc98e6ff5b595724
fsorodrigues <fsorodrigues@gmail.com>
2026-09-09 00:03:26 +1200

    feat(nvim/conform): improve conform auto-save

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,21 +1,29 @@
 return {
   "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      sql = { "sqlfluff" },
-      lua = { "stylua", lsp_format = "fallback" },
-      go = { "gofumpt", "goimports", "golines", lsp_format = "fallback" },
-    },
-    formatters = {
-      sqlfluff = {
-        command = "sqlfluff",
-        args = { "fix", "--force", "--quiet", "$FILENAME" },
-        stdin = false,
+  opts = function()
+    return {
+      formatters_by_ft = {
+        sql = { "sqlfluff" },
+        lua = { "stylua", lsp_format = "fallback" },
+        go = { "gofumpt", "goimports", "golines", lsp_format = "fallback" },
       },
-    },
-    format_on_save = {
-      timeout_ms = 10000,
-    },
-    default_format_opts = { async = true },
-  },
+      formatters = {
+        sqlfluff = {
+          -- command = "sqlfluff",
+          -- args = { "fix", "--force", "--quiet", "$FILENAME" },
+          -- stdin = false,
+          cwd = require("conform.util").root_file({
+            ".sqlfluff",
+            "pyproject.toml",
+            "setup.cfg",
+          }),
+        },
+      },
+      format_on_save = {
+        timeout_ms = 10000,
+      },
+      default_format_opts = { async = true },
+    }
+  end,
 }
+
